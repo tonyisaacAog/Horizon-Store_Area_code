7f5eb7ab5926210da2c8da48927bb86657e9f9f3
Cyrus <ahmeedsleem8@gmail.com>
2025-08-25 10:49:30 +0300

    work new requirement:

--- Horizon/obj/Debug/net6.0/Horizon.AssemblyInfo.cs
+++ Horizon/obj/Debug/net6.0/Horizon.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Horizon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e0349277de3cba3c416ba94d594347b940b3431")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4874a351e4ac3566e373b8f9c65c13970f25886")]
 [assembly: System.Reflection.AssemblyProductAttribute("Horizon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Horizon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
